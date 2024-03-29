4919c21fd810ef280470d0325956036580676d26
demo-23home <zeyadslama23@gmail.com>
2024-03-29 16:06:25 +0200

    test

--- database/migrations/2024_03_10__000002_create_alarms_table.php
+++ database/migrations/2024_03_10__000002_create_alarms_table.php
@@ -1,32 +1,33 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAlarmsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    // /
+    //  * Run the migrations.
+    //  */
+    public function up(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::create('alarms', function (Blueprint $table) {
             $table->id()->unsigned()->foreign('Doctor.Alarm_id');
             $table->string('Message');
             $table->timestamp('Time');
+            //$table->foreignId('doctor_id')->constrained()->onDelete('cascade');
         });
-
-        // // Create foreign key constraint separately
-        // Schema::table('alarms', function (Blueprint $table) {
-        //     $table->foreignId('doctor_id')
-        //           ->constrained('doctors')
-        //           ->onDelete('cascade');
-        // });
+        Schema::enableForeignKeyConstraints();
     }
 
-    public function down()
+    // /
+    //  * Reverse the migrations.
+    //  */
+    public function down(): void
     {
-        Schema::table('alarms', function (Blueprint $table) {
-            $table->dropForeign(['doctor_id']);
-        });
-
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('alarms');
+        Schema::enableForeignKeyConstraints(); 
     }
-}
+};
